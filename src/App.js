459043401f0129ee454bcc056fae84f8e1cd7ceb
Cook1619 <cook6348@gmail.com>
2018-08-20 07:25:21 -0500

    Responsive navbar addded

--- src/App.js
+++ src/App.js
@@ -8,18 +8,27 @@ class App extends Component {
     return (
       <Router>
         <Fragment>
-          <nav>
-            <ul className="navbar-nav">
-              <li lassName="nav-item">
-                <Link className="btn" to="/">Home Page</Link>
-              </li>
-              <li lassName="nav-item">
-                <Link to="/about">About</Link>
-              </li>
-              <li lassName="nav-item">
-                <Link to="/resume">Resume</Link>
-              </li>
-            </ul>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <a class="navbar-brand" href="#">Navbar w/ text</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+              </ul>
+              <span class="navbar-text">
+                Navbar text with an inline element
+    </span>
+            </div>
           </nav>
           <Switch>
             <Route exact path="/" component={HomePage} />
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,39 +6,39 @@ const Technologies = () => {
         <div>
             <section className="packages">
                 <h3 className="title">Technolgies Known</h3>
-                <p>We offer a variety of mountaineering packages. Whether you've climbed Everest or don't even know what a mountain is, we've got the perfect vacation for you.</p>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
                 <hr />
 
                 <ul className="grid">
                     <li>
                         <i className="fab fa-js-square icon-size" />
                         <h4>JavaScript</h4>
-                        <p>Looking for the complete experience? Take a tour with one of our experts. They'll show you secrets that you're likely to miss otherwise.</p>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
                     </li>
                     <li>
                         <i className="fab fa-react icon-size" />
                         <h4>React</h4>
-                        <p>Want to experience nature's beauty without all of that annoying exercise? Take a photo tour on one of our mountain buses.</p>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old..</p>
                     </li>
                     <li>
                         <i className="fab fa-node icon-size" />
                         <h4>Node JS</h4>
-                        <p>If bicycles are more your speed, consider taking a tour through one of our mountain bike paths. We'll provide the bikes, and lunch too!</p>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
                     </li>
                     <li>
                         <i className="fas fa-database icon-size" />
                         <h4>MySQL</h4>
-                        <p>Got a competitive spirit? Sign up for one of our mountain marathons! Try to reach the summit before anyone else.</p>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
                     </li>
                     <li>
                         <i className="fab fa-aws icon-size" />
                         <h4>AWS</h4>
-                        <p>Got a competitive spirit? Sign up for one of our mountain marathons! Try to reach the summit before anyone else.</p>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
                     </li>
                     <li>
                         <i className="fab fa-github icon-size" />
                         <h4>GitHub</h4>
-                        <p>Got a competitive spirit? Sign up for one of our mountain marathons! Try to reach the summit before anyone else.</p>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
                     </li>
                 </ul>
             </section>
